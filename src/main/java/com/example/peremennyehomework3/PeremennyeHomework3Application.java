2e8597676b335c7282bfb7c5e541a5fb4efece5a
Alexeybroadway <johnclevervoice@gmail.com>
2022-09-23 21:00:41 +0500

    Initial commit

---
+++ src/main/java/com/example/peremennyehomework3/PeremennyeHomework3Application.java
@@ -0,0 +1,13 @@
+package com.example.peremennyehomework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeremennyeHomework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeremennyeHomework3Application.class, args);
+    }
+
+}
